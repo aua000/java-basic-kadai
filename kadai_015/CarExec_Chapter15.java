d6dbe303dc2f3bc92b682c3b0523ae85c7745375
aua000 <asukahrs@gmail.com>
2025-04-01 10:42:32 +0900

    再提出

--- kadai_015/CarExec_Chapter15.java
+++ kadai_015/CarExec_Chapter15.java
@@ -1,16 +1,16 @@
-package kadai_15;
+package kadai_015;
 
 public class CarExec_Chapter15 {
 
 	public static void main(String[] args) {
 		// 車クラスのインスタンスを作成する
-		Car_Chapter15 Car = new Car_Chapter15(1,10);
+		Car_Chapter15 car = new Car_Chapter15();
 		
 		//車クラスのgearChangeメソッドを実行する
-		Car.gearChange(3);
+		car.gearChange(3);
 		
 		//車クラスのrunメソッドを実行する
-		Car.run();
+		car.run();
 	}
 
 }
--- kadai_015/Car_Chapter15.java
+++ kadai_015/Car_Chapter15.java
@@ -1,28 +1,27 @@
-package kadai_15;
+package kadai_015;
 
 public class Car_Chapter15 {
 	
 	private int gear = 1;
 	private int speed =10;
 
-	public Car_Chapter15(int gear,int speed) {
-		this.gear = gear;
-		this.speed = speed;
+	public Car_Chapter15() {
+		
 	}
 	
 	//ギアの値により速度を変える
 	public void gearChange(int afterGear) {
 		
-		int beforGear = this.gear;
-		this.gear = afterGear;
+		int beforGear = gear;
+		gear = afterGear;
 		
 		switch (afterGear) {
-			case 1 -> this.speed = 10;
-			case 2 -> this.speed = 20;
-			case 3 -> this.speed = 30;
-			case 4 -> this.speed = 40;
-			case 5 -> this.speed = 50;
-			default -> this.speed = 10;
+			case 1 -> speed = 10;
+			case 2 -> speed = 20;
+			case 3 -> speed = 30;
+			case 4 -> speed = 40;
+			case 5 -> speed = 50;
+			default -> speed = 10;
 			
 		}
 		
@@ -32,6 +31,6 @@ public class Car_Chapter15 {
 	
 	//ギアチェンジ後の速度を表示する
 	public void run() {
-		System.out.println("速度は時速" + this.speed + "kmです");
+		System.out.println("速度は時速" + speed + "kmです");
 	}
 }
